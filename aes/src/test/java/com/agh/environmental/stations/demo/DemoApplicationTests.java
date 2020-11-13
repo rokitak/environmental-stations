54b7d5225a11681bd25894d256a8e005e877d254
Krystian Rokita <krokita@consol.de>
2020-11-13 19:50:02 +0100

    second commit

---
+++ aes/src/test/java/com/agh/environmental/stations/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.environmental.stations.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
